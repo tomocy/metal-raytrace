15208fd05d6039a61cc13635a23aff8a9f9ef43f
tomocy <tomocy.dev@gmail.com>
2024-04-05 15:28:58 +0900

    Let Engine/View add a metallic golden sphere

--- Raytrace/Engine/Engine+View.swift
+++ Raytrace/Engine/Engine+View.swift
@@ -24,33 +24,94 @@ extension Engine {
             colorPixelFormat = .rgba8Unorm_srgb
             shader = try! .init(device: device, resolution: drawableSize, format: colorPixelFormat)
 
-            meshes = []
             do {
-                let raw = MDLMesh.init(
-                    try! .load(
-                        url: Bundle.main.url(
-                            forResource: "Spot",
-                            withExtension: "obj",
-                            subdirectory: "Farm/Spot"
-                        )!,
-                        with: device
-                    ).first!,
-                    indexType: .uint16
-                )
+                meshes = []
 
                 do {
+                    let raw = MDLMesh.init(
+                        try! .load(
+                            url: Bundle.main.url(
+                                forResource: "Spot",
+                                withExtension: "obj",
+                                subdirectory: "Farm/Spot"
+                            )!,
+                            with: device
+                        ).first!,
+                        indexType: .uint16
+                    )
+
+                    do {
+                        var mesh = try! raw.toMesh(
+                            with: device,
+                            instances: [
+                                .init(
+                                    transform: .init(
+                                        translate: .init(-0.5, 0, 0)
+                                    )
+                                ),
+                            ]
+                        )
+                        mesh.pieces[0].material = .init(
+                            albedo: mesh.pieces[0].material?.albedo,
+                            metalness: try! Shader.Texture.fill(
+                                .init(red: 1, green: 0, blue: 0, alpha: 0),
+                                with: device
+                            )
+                        )
+
+                        meshes!.append(mesh)
+                    }
+
+                    do {
+                        var mesh = try! raw.toMesh(
+                            with: device,
+                            instances: [
+                                .init(
+                                    transform: .init(
+                                        translate: .init(0.5, 0, 0)
+                                    )
+                                ),
+                            ]
+                        )
+                        mesh.pieces[0].material = .init(
+                            albedo: mesh.pieces[0].material?.albedo,
+                            metalness: try! Shader.Texture.fill(
+                                .init(red: 0, green: 0, blue: 0, alpha: 0),
+                                with: device
+                            )
+                        )
+
+                        meshes!.append(mesh)
+                    }
+                }
+
+                do {
+                    let raw = MDLMesh.init(
+                        .init(
+                            sphereWithExtent: .init(0.4, 0.4, 0.4),
+                            segments: .init(24, 24),
+                            inwardNormals: false,
+                            geometryType: .triangles,
+                            allocator: MTKMeshBufferAllocator.init(device: device)
+                        ),
+                        indexType: .uint16
+                    )
+
                     var mesh = try! raw.toMesh(
                         with: device,
                         instances: [
                             .init(
                                 transform: .init(
-                                    translate: .init(-0.5, 0, 0)
+                                    translate: .init(0, 0.8, 0.8)
                                 )
                             ),
                         ]
                     )
                     mesh.pieces[0].material = .init(
-                        albedo: mesh.pieces[0].material?.albedo,
+                        albedo: try! Shader.Texture.fill(
+                            .init(red: 1, green: 0.75, blue: 0.25, alpha: 1),
+                            with: device
+                        ),
                         metalness: try! Shader.Texture.fill(
                             .init(red: 1, green: 0, blue: 0, alpha: 0),
                             with: device
@@ -61,18 +122,31 @@ extension Engine {
                 }
 
                 do {
+                    let raw = MDLMesh.init(
+                        .init(
+                            planeWithExtent: .init(4, 0, 4),
+                            segments: .init(1, 1),
+                            geometryType: .triangles,
+                            allocator: MTKMeshBufferAllocator.init(device: device)
+                        ),
+                        indexType: .uint16
+                    )
+
                     var mesh = try! raw.toMesh(
                         with: device,
                         instances: [
                             .init(
                                 transform: .init(
-                                    translate: .init(0.5, 0, 0)
+                                    translate: .init(0, 0, 0)
                                 )
                             ),
                         ]
                     )
                     mesh.pieces[0].material = .init(
-                        albedo: mesh.pieces[0].material?.albedo,
+                        albedo: try! Shader.Texture.load(
+                            from: Bundle.main.url(forResource: "Ground", withExtension: "png", subdirectory: "Farm/Ground")!,
+                            with: device
+                        ),
                         metalness: try! Shader.Texture.fill(
                             .init(red: 0, green: 0, blue: 0, alpha: 0),
                             with: device
@@ -81,41 +155,6 @@ extension Engine {
 
                     meshes!.append(mesh)
                 }
-            }
-
-            do {
-                let raw = MDLMesh.init(
-                    .init(
-                        planeWithExtent: .init(4, 0, 4),
-                        segments: .init(1, 1),
-                        geometryType: .triangles,
-                        allocator: MTKMeshBufferAllocator.init(device: device)
-                    ),
-                    indexType: .uint16
-                )
-
-                var mesh = try! raw.toMesh(
-                    with: device,
-                    instances: [
-                        .init(
-                            transform: .init(
-                                translate: .init(0, 0, 0)
-                            )
-                        ),
-                    ]
-                )
-                mesh.pieces[0].material = .init(
-                    albedo: try! Shader.Texture.load(
-                        from: Bundle.main.url(forResource: "Ground", withExtension: "png", subdirectory: "Farm/Ground")!,
-                        with: device
-                    ),
-                    metalness: try! Shader.Texture.fill(
-                        .init(red: 0, green: 0, blue: 0, alpha: 0),
-                        with: device
-                    )
-                )
-
-                meshes!.append(mesh)
             }
 
             renderFrame = .init(id: 0)
