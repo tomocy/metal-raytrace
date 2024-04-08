d99686cf1827c2a8d6b10894de69783e368157ea
tomocy <tomocy.dev@gmail.com>
2024-04-08 20:41:55 +0900

    Add Prelight/Kernel

--- Raytrace/Prelight/Diffuse.swift
+++ Raytrace/Prelight/Diffuse.swift
@@ -4,8 +4,7 @@ import Metal
 
 extension Prelight {
     struct Diffuse {
-        private var pipelineStates: PipelineStates
-        private var args: Args
+        private var kernel: Kernel
 
         private var source: any MTLTexture
         private(set) var target: any MTLTexture
@@ -17,12 +16,7 @@ extension Prelight.Diffuse {
         let lib = device.makeDefaultLibrary()!
         let fn = lib.makeFunction(name: "Prelight::Diffuse::compute")!
 
-        pipelineStates = .init(
-            compute: try PipelineStates.make(with: device, for: fn)
-        )
-        args = .init(
-            encoder: Args.make(for: fn)
-        )
+        kernel = try .init(device: device, label: "Diffuse", function: fn)
 
         do {
             self.source = source
@@ -43,80 +37,6 @@ extension Prelight.Diffuse {
 
 extension Prelight.Diffuse {
     func encode(to buffer: some MTLCommandBuffer) {
-        let encoder = buffer.makeComputeCommandEncoder()!
-        defer { encoder.endEncoding() }
-
-        encoder.setComputePipelineState(pipelineStates.compute)
-
-        do {
-            let buffer = args.build(source, target, with: encoder)!
-            encoder.setBuffer(buffer, offset: 0, index: 0)
-        }
-
-        do {
-            let threadsSizePerGroup = encoder.defaultThreadsSizePerGroup
-            let threadsGroupSize = encoder.threadsGroupSize(
-                for: .init(target.width, target.height),
-                as: threadsSizePerGroup
-            )
-
-            encoder.dispatchThreadgroups(
-                threadsGroupSize,
-                threadsPerThreadgroup: threadsSizePerGroup
-            )
-        }
-    }
-}
-
-extension Prelight.Diffuse {
-    struct PipelineStates {
-        var compute: any MTLComputePipelineState
-    }
-}
-
-extension Prelight.Diffuse.PipelineStates {
-    static func make(with device: some MTLDevice, for function: some MTLFunction) throws -> any MTLComputePipelineState {
-        return try device.makeComputePipelineState(
-            function: function
-        )
-    }
-}
-
-extension Prelight.Diffuse {
-    struct Args {
-        var encoder: any MTLArgumentEncoder
-    }
-}
-
-extension Prelight.Diffuse.Args {
-    static func make(for function: some MTLFunction) -> any MTLArgumentEncoder {
-        return function.makeArgumentEncoder(bufferIndex: 0)
-    }
-}
-
-extension Prelight.Diffuse.Args {
-    func build(
-        _ source: some MTLTexture,
-        _ target: some MTLTexture,
-        with encoder: some MTLComputeCommandEncoder
-    ) -> (any MTLBuffer)? {
-        guard let buffer = encoder.device.makeBuffer(
-            length: self.encoder.encodedLength
-        ) else { return nil }
-
-        buffer.label = "Prelight/Diffuse/Args"
-
-        self.encoder.setArgumentBuffer(buffer, offset: 0)
-
-        do {
-            encoder.useResource(source, usage: .read)
-            self.encoder.setTexture(source, index: 0)
-        }
-        do {
-            encoder.useResource(target, usage: .write)
-            self.encoder.setTexture(target, index: 1)
-        }
-
-        return buffer
+        kernel.encode(to: buffer, source: source, target: target)
     }
 }
---
+++ Raytrace/Prelight/Kernel.swift
@@ -0,0 +1,105 @@
+// tomocy
+
+import Metal
+
+extension Prelight {
+    struct Kernel {
+        var label: String
+        private var pipelineStates: PipelineStates
+        private var args: Args
+    }
+}
+
+extension Prelight.Kernel {
+    init(device: some MTLDevice, label: String, function: some MTLFunction) throws {
+        self.label = label
+
+        pipelineStates = .init(
+            compute: try PipelineStates.make(with: device, for: function)
+        )
+        args = .init(
+            encoder: Args.make(for: function)
+        )
+    }
+}
+
+extension Prelight.Kernel {
+    func encode(to buffer: some MTLCommandBuffer, source: some MTLTexture, target: some MTLTexture) {
+        let encoder = buffer.makeComputeCommandEncoder()!
+        defer { encoder.endEncoding() }
+
+        encoder.setComputePipelineState(pipelineStates.compute)
+
+        do {
+            let buffer = args.build(source, target, with: encoder, label: "Prelight/\(label)/Args")!
+            encoder.setBuffer(buffer, offset: 0, index: 0)
+        }
+
+        do {
+            let threadsSizePerGroup = encoder.defaultThreadsSizePerGroup
+            let threadsGroupSize = encoder.threadsGroupSize(
+                for: .init(target.width, target.height),
+                as: threadsSizePerGroup
+            )
+
+            encoder.dispatchThreadgroups(
+                threadsGroupSize,
+                threadsPerThreadgroup: threadsSizePerGroup
+            )
+        }
+    }
+}
+
+extension Prelight.Kernel {
+    struct PipelineStates {
+        var compute: any MTLComputePipelineState
+    }
+}
+
+extension Prelight.Kernel.PipelineStates {
+    static func make(with device: some MTLDevice, for function: some MTLFunction) throws -> any MTLComputePipelineState {
+        return try device.makeComputePipelineState(
+            function: function
+        )
+    }
+}
+
+extension Prelight.Kernel {
+    struct Args {
+        var encoder: any MTLArgumentEncoder
+    }
+}
+
+extension Prelight.Kernel.Args {
+    static func make(for function: some MTLFunction) -> any MTLArgumentEncoder {
+        return function.makeArgumentEncoder(bufferIndex: 0)
+    }
+}
+
+extension Prelight.Kernel.Args {
+    func build(
+        _ source: some MTLTexture,
+        _ target: some MTLTexture,
+        with encoder: some MTLComputeCommandEncoder,
+        label: String
+    ) -> (any MTLBuffer)? {
+        guard let buffer = encoder.device.makeBuffer(
+            length: self.encoder.encodedLength
+        ) else { return nil }
+
+        buffer.label = label
+
+        self.encoder.setArgumentBuffer(buffer, offset: 0)
+
+        do {
+            encoder.useResource(source, usage: .read)
+            self.encoder.setTexture(source, index: 0)
+        }
+        do {
+            encoder.useResource(target, usage: .write)
+            self.encoder.setTexture(target, index: 1)
+        }
+
+        return buffer
+    }
+}
