36369b6d2dc72a6be3f15e5244af25c70504f6d4
tomocy <tomocy.dev@gmail.com>
2024-04-11 05:36:22 +0900

    Add App/Engine

---
+++ Raytrace/App/Engine/Engine+View.swift
@@ -0,0 +1,33 @@
+// tomocy
+
+import Metal
+import MetalKit
+
+extension Engine {
+    class View: MTKView {
+        required init(coder: NSCoder) { super.init(coder: coder) }
+
+        init(
+            device: some MTLDevice,
+            size: CGSize
+        ) {
+            super.init(
+                frame: .init(
+                    origin: .init(x: 0, y: 0),
+                    size: size
+                ),
+                device: device
+            )
+
+            delegate = self
+
+            colorPixelFormat = .rgba8Unorm_srgb
+        }
+    }
+}
+
+extension Engine.View: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
+
+    func draw(in view: MTKView) {}
+}
